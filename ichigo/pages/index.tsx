13057c3911b89ff1ade2c8ddc790586a2a457128
Xavier Fernandez <lain@MacBook-Pro-3.home>
2021-10-14 01:41:08 -0400

    first commit

---
+++ ichigo/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+      <div className="absolute top-0 left-0 bottom-0 right-0 text-red-400 flex items-center justify-center">hi</div>
+  )
+}
+
+export default Home
